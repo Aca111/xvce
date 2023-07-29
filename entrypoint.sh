dc713c2852f8ecaf0855fa3fda90614b49980ab0
xx@xx <xx@xx>
2023-07-29 22:56:23 +0330

    update2

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,8 +6,7 @@ timeout 4s nginx -g "daemon off;"
 # starting xray-core
 sleep 5s && xray run -config /etc/xray/config.json
 #netstat to see if xray is running on port 80
-ps && netstat -tupln
-
+watch ps 
 
 #/usr/sbin/sshd
 #/usr/bin/shellinaboxd -t

0d4d6aafd265fa580fa58018593e9cf05e7ac705
xx@xx <xx@xx>
2023-07-30 17:48:58 +0330

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
-#starting shellinabox 
-/usr/bin/shellinaboxd -p 3122 -t
 #starting nginx 
 nginx -g "daemon off;"
+
+#starting shellinabox 
+/usr/bin/shellinaboxd -p 3122 -t
+
 # starting xray-core
 #xray run -config /etc/xray/config.json
 

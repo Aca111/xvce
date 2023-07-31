7d55a41264423ad0700ebbfa9fb56a5ba074e63e
xx@xx <xx@xx>
2023-07-31 15:07:58 +0330

    update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 #starting nginx 
-timeout 20s nginx -g "daemon off;" && xray run -config /etc/xray/config.json
+timeout 1m nginx -g "daemon off;"
 #starting shellinabox 
 #/usr/bin/shellinaboxd -p 3122 -t
 # starting xray-core
+sleep 1.1m && xray run -config /etc/xray/config.json
